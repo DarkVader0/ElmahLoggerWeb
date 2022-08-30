ca1527781563359b1b675631133b87becc3fa9dd
radivoje.milutinovic <radivoje.milutinovic@dilig.net>
2022-08-30 10:27:26 +0200

    Changed ports, added decorator to request

--- ElmahLoggerWeb.Lambda/Contracts/Requests/LogRequest.cs
+++ ElmahLoggerWeb.Lambda/Contracts/Requests/LogRequest.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json.Serialization;
+using FastEndpoints;
 
 namespace ElmahLoggerWeb.Lambda.Contracts.Requests;
 
 public class LogRequest
 {
-    [FastEndpoints.FromHeader("api_key")] public string ApiKey { get; init; } = default!;
+    [FromHeader("api_key")] 
+    public string ApiKey { get; init; } = default!;
+    
+    [JsonPropertyName("message")]
     public string Message { get; init; } = default!;
 }
